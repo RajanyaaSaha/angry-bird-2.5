e4c4cdc5b17498721c13a7191534990fd97f24f7
RajanyaaSaha <rajanyaa2020@gmail.com>
2021-02-21 20:38:44 +0530

    add platform2

--- sketch.js
+++ sketch.js
@@ -17,7 +17,8 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform1= new Ground(150, 305, 300, 170);
+    platform2 =new Ground(100,200,200,140);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -60,5 +61,7 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+    platform1.display();
+    platform2.display();
+    
 }
